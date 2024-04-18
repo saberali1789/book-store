2e1516c8b010562c6e0825c7b935444b45747a79
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2024-04-18 18:04:45 +0200

    Cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,78 @@
-
-
+import { cartInfo } from "../../data/cart";
+import "./cart.css";
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
 
-export default Cart
+  const totalPrice = cartInfo.reduce((acc, cur) => acc + cur.price * cur.quantity, 0)
+  return (
+    <div className="cart">
+      <div className="cart-title">Your Shopping Cart</div>
+      <div className="cart-wrapper">
+        <div className="cart-items">
+          {cartInfo.map((item) => (
+            <div key={item.id} className="cart-item">
+              <img
+                src={`/books/${item.image}`}
+                alt={item.title}
+                className="cart-item-img"
+              />
+              <div className="cart-item-info">
+                <div>
+                  <div className="cart-item-book-title">
+                    <b>Title: </b>
+                    {item.title}
+                  </div>
+                  <div className="cart-item-author">
+                    <b>Author: </b>
+                    {item.author}
+                  </div>
+                </div>
+                <div>
+                  <div className="cart-item-quantity">
+                    <button>
+                      <i className="bi bi-plus-lg"></i>
+                    </button>
+                    <b>{item.quantity}</b>
+                    <button>
+                      <i className="bi bi-dash-lg"></i>
+                    </button>
+                  </div>
+                  <div className="cart-item-price">
+                    ${item.price * item.quantity}
+                  </div>
+                  <i className="bi bi-trash-fill"></i>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="cart-order-summary">
+          <div className="order-summary-title">ORDER SUMMARY</div>
+          <div className="order-summary-item">
+            <span>Subtotal</span>
+            <span>
+              $
+              {totalPrice}
+            </span>
+          </div>
+          <div className="order-summary-item">
+            <span>Shopping Cost</span>
+            <span>0 </span>
+          </div>
+          <div className="order-summary-item">
+            <span>Discount</span>
+            <span>$ 0 </span>
+          </div>
+          <div className="order-summary-item">
+            <span>Total</span>
+            <span>
+              $
+              {totalPrice}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
